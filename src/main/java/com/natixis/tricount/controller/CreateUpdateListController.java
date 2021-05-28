093192da170a4812b316a42ed54569c634a23ca9
ergunmu1 <mustafa.ergun@natixis.com>
2021-05-28 17:06:06 +0200

    commit create update List

---
+++ src/main/java/com/natixis/tricount/controller/CreateUpdateListController.java
@@ -0,0 +1,45 @@
+package com.natixis.tricount.controller;
+
+import com.natixis.tricount.service.CreateUpdateListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
+
+@Controller
+public class CreateUpdateListController
+{
+    @Autowired
+    private CreateUpdateListService createUpdateListService ;
+
+    @GetMapping("/lists/{idList}")
+    public String createUpdateList(Model model, @PathVariable(required = false) String idList)
+    {
+        if (idList != null)
+        {
+//            Optional<School> optionalWizard = schoolRepository.findById(id);
+//            if (optionalWizard.isPresent()) {
+//                school = optionalWizard.get();
+//            }
+        }
+
+
+        return "createUpdateList";
+    }
+
+    @PostMapping("/lists")
+    public String createUpdateList (@RequestParam Long idList)
+    {
+        if (idList != null)
+        {
+
+        }
+        createUpdateListService.save(idList);
+        return "redirect:/lists";
+    }
+}
---
+++ src/main/java/com/natixis/tricount/service/CreateUpdateListService.java
@@ -0,0 +1,16 @@
+package com.natixis.tricount.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CreateUpdateListService {
+
+    //@Autowired
+    // private CreateUpdateListService createUpdateListService ;
+    // à ajouter le repo vers ExpenseListRepository
+
+    public void save(Long idList) {
+        //repo.save(idList);
+    }
+}
