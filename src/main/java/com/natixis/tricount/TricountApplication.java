b305dba7c35708eb85effa9039b0ee0c53a09ad0
ergunmu1 <mustafa.ergun@natixis.com>
2021-05-28 09:57:07 +0200

    first project commit Mustafa

---
+++ src/main/java/com/natixis/tricount/TricountApplication.java
@@ -0,0 +1,13 @@
+package com.natixis.tricount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TricountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TricountApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/natixis/tricount/TricountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natixis.tricount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TricountApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
