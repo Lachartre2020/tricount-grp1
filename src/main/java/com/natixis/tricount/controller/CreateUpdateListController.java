300af7b771371bf08d8eb9d424ed0c1b9e647cb7
ergunmu1 <mustafa.ergun@natixis.com>
2021-06-10 10:03:54 +0200

    update page modification d'une liste de dépense

--- src/main/java/com/natixis/tricount/controller/CreateUpdateListController.java
+++ src/main/java/com/natixis/tricount/controller/CreateUpdateListController.java
@@ -1,13 +1,12 @@
 package com.natixis.tricount.controller;
 
+import com.natixis.tricount.entity.ExpenseList;
 import com.natixis.tricount.service.CreateUpdateListService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -18,28 +17,41 @@ public class CreateUpdateListController
     private CreateUpdateListService createUpdateListService ;
 
     @GetMapping("/lists/{idList}")
-    public String createUpdateList(Model model, @PathVariable(required = false) String idList)
+    public String createUpdateList(Model model, @PathVariable(required = false) Long idList)
     {
+        ExpenseList expenseList = new ExpenseList();
         if (idList != null)
         {
-//            Optional<School> optionalWizard = schoolRepository.findById(id);
-//            if (optionalWizard.isPresent()) {
-//                school = optionalWizard.get();
-//            }
+            Optional<ExpenseList> OptionalExpenseList =createUpdateListService.findById(idList);
+            if (OptionalExpenseList.isPresent()) {
+                expenseList = OptionalExpenseList.get();
+            }
         }
-
-
+        model.addAttribute("expenseList", expenseList);
         return "createUpdateList";
     }
 
-    @PostMapping("/lists")
-    public String createUpdateList (@RequestParam Long idList)
-    {
-        if (idList != null)
-        {
-
-        }
-        createUpdateListService.save(idList);
-        return "redirect:/lists";
+    @PostMapping("/lists/{idList}")
+    public String createUpdateList(@ModelAttribute ExpenseList expenseList) {
+        createUpdateListService.save(expenseList);
+        return "redirect:/lists/"+expenseList.getId();
     }
+
+    @GetMapping("/listss")
+    public String createUpdateListGet(Model model)
+    {
+        ExpenseList expenseList = new ExpenseList();
+        model.addAttribute("expenseList", expenseList);
+        return "createUpdateList";
+    }
+
+//    @PostMapping("/lists")
+//    public String createList (ExpenseList expenseList)
+//    {
+//        expenseList.getName();
+//
+//        createUpdateListService.save(expenseList);
+//
+//        return "createUpdateList";
+//    }
 }
--- src/main/java/com/natixis/tricount/service/CreateUpdateListService.java
+++ src/main/java/com/natixis/tricount/service/CreateUpdateListService.java
@@ -1,8 +1,11 @@
 package com.natixis.tricount.service;
 
+import com.natixis.tricount.entity.ExpenseList;
 import com.natixis.tricount.repository.ExpenseListRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CreateUpdateListService {
@@ -10,7 +13,11 @@ public class CreateUpdateListService {
     @Autowired
     private ExpenseListRepository expenseListRepository;
 
-    public void save(Long idList) {
-        //repo.save(idList);
+    public void save(ExpenseList expenseList) {
+        expenseListRepository.save(expenseList);
+    }
+
+    public Optional<ExpenseList> findById(Long idList) {
+        return expenseListRepository.findById(idList);
     }
 }
