1f8996c91feb834e8d85efe5cdbf791114344c0a
ergunmu1 <mustafa.ergun@natixis.com>
2021-06-10 11:02:02 +0200

    update page modification d'une liste de dépense pour ajout d'un participant. -Création Repo et Entity Participant

--- src/main/java/com/natixis/tricount/entity/ExpenseList.java
+++ src/main/java/com/natixis/tricount/entity/ExpenseList.java
@@ -1,9 +1,7 @@
 package com.natixis.tricount.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 public class ExpenseList {
@@ -12,6 +10,17 @@ public class ExpenseList {
     private Long id;
 
     private String name;
+
+    @OneToMany(mappedBy = "expenseList")
+    List<Participant> participants;
+
+    public List<Participant> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(List<Participant> participants) {
+        this.participants = participants;
+    }
 
     public void setId(Long id) {
         this.id = id;
--- src/main/java/com/natixis/tricount/service/CreateUpdateListService.java
+++ src/main/java/com/natixis/tricount/service/CreateUpdateListService.java
@@ -1,10 +1,13 @@
 package com.natixis.tricount.service;
 
 import com.natixis.tricount.entity.ExpenseList;
+import com.natixis.tricount.entity.Participant;
 import com.natixis.tricount.repository.ExpenseListRepository;
+import com.natixis.tricount.repository.ParticpantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -13,11 +16,23 @@ public class CreateUpdateListService {
     @Autowired
     private ExpenseListRepository expenseListRepository;
 
+    @Autowired
+    private ParticpantRepository participantRepository;
+
     public void save(ExpenseList expenseList) {
         expenseListRepository.save(expenseList);
     }
 
     public Optional<ExpenseList> findById(Long idList) {
         return expenseListRepository.findById(idList);
+    }
+
+    public void addParticipantList(Participant participant, Long idList)
+    {
+        Optional<ExpenseList> expenseList = expenseListRepository.findById(idList);
+        List<Participant> participantsExpenseList = expenseList.get().getParticipants();
+        participantsExpenseList.add(participant);
+        expenseListRepository.save(expenseList.get());
+        //participantRepository.save(participant);
     }
 }
