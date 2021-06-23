ccac8c695b04db73bd8c9e45870da8ef77b5b999
Lachartre2020 <s.lachartre@hotmail.fr>
2021-06-23 10:01:11 +0200

    ajt Entity Expense, Participant

--- src/main/java/com/natixis/tricount/entity/Expense.java
+++ src/main/java/com/natixis/tricount/entity/Expense.java
@@ -1,52 +1,80 @@
 package com.natixis.tricount.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.*;
 
 @Entity
 public class Expense {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    private String name;
-
-    private Long amount;
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private Float amount;
+	
     @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.REFRESH, optional = false)
     @JoinColumn(name = "expense_list_id", nullable = false)
     private ExpenseList expenseList;
+	
+	@ManyToOne
+	@JoinColumn(name = "participant_id")
+	private Participant participant;
 
+	@ManyToMany(mappedBy = "expenses")
+	private List<Participant> participants = new ArrayList<>();
+	
+	public Expense() {
+		
+	}
+	
+	public Long getId() {
+		return this.id;
+	}
+	
+	public void setId(Long id) {
+		this.id = id;
+	}
+		
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Float getAmount() {
+		return this.amount;
+	}
+	
+	public void setAmount(Float amount) {
+		this.amount = amount;
+	}
+	
+	public ExpenseList getExpenselist() {
+		return this.expenseList;
+	}
+	
+	public void setExpenseList(ExpenseList expenseList) {
+		this.expenseList = expenseList;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public Participant getParticipant() {
+		return this.participant;
+	}
+	
+	public void setParticipant(Participant participant) {
+		this.participant = participant;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Long getAmount() {
-        return amount;
-    }
-
-    public void setAmount(Long amount) {
-        this.amount = amount;
-    }
-
-    public ExpenseList getExpenseList() {
-        return expenseList;
-    }
-
-    public void setExpenseList(ExpenseList expenseList) {
-        this.expenseList = expenseList;
-    }
+	public List<Participant> getParticipants() {
+		return this.participants;
+	}
+	
+	public void setParticipants(List<Participant> participants) {
+		this.participants = participants;
+	}	
+	
 }
