e9190c9335c65b23c578956d8bd6473c993d5c3b
christopheFa <christophe.faivre@natixis.com>
2021-07-09 19:10:28 +0200

    Ajout Pagination dans liste de dépenses

--- src/main/java/com/natixis/tricount/controller/ExpenseListController.java
+++ src/main/java/com/natixis/tricount/controller/ExpenseListController.java
@@ -1,6 +1,8 @@
 package com.natixis.tricount.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,6 +11,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.natixis.tricount.entity.ExpenseList;
 import com.natixis.tricount.service.ExpenseListService;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 @Controller
 public class ExpenseListController {
@@ -17,8 +25,26 @@ public class ExpenseListController {
 	private ExpenseListService service;
 	
 	@GetMapping("/lists")
-	public String getAllExpenseLists(Model model) {
-		model.addAttribute("expenseList",service.findAll() );
+	public String getAllExpenseLists(Model model,
+									 @RequestParam("page") Optional<Integer> page,
+									 @RequestParam("size") Optional<Integer> size) {
+
+		int currentPage = page.orElse(1);
+		int pageSize = size.orElse(15);
+
+		Page<ExpenseList> expensiveListPage = service.findPaginated(PageRequest.of(currentPage - 1, pageSize));
+		model.addAttribute("expenseList",expensiveListPage);
+
+		int totalPages = expensiveListPage.getTotalPages();
+		int firstPageNo = Math.max(2, currentPage - 2);
+		int lastPageNo = Math.min(totalPages - 1, currentPage + 2);
+		if (totalPages > 0) {
+			model.addAttribute("currentPage", currentPage);
+			model.addAttribute("totalPages",  totalPages);
+			model.addAttribute("firstPageNo",  firstPageNo);
+			model.addAttribute("lastPageNo",  lastPageNo);
+		}
+
 		return "lists";
 	}
 
