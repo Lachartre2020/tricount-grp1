d57e135c0bfd463d5fbf40bc24c6bb2b726fbf11
levequesa <79331585+levequesa@users.noreply.github.com>
2021-07-23 10:56:05 +0200

    variablisation fragment et gestion des pages sur les dépenses

--- src/main/java/com/natixis/tricount/repository/ExpenseListRepository.java
+++ src/main/java/com/natixis/tricount/repository/ExpenseListRepository.java
@@ -1,6 +1,11 @@
 package com.natixis.tricount.repository;
 
 import com.natixis.tricount.entity.ExpenseList;
+
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
