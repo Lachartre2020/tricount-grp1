e46058f2f6761a1a41c726b2e3a8216967a97aad
ergunmu1 <mustafa.ergun@natixis.com>
2021-07-23 11:20:20 +0200

    configuration sendmail (html service dto...)

--- src/main/java/com/natixis/tricount/mail/Mail.java
+++ src/main/java/com/natixis/tricount/mail/Mail.java
@@ -6,7 +6,12 @@ import com.natixis.tricount.entity.ExpenseList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.util.List;
 
 @Component
 public class Mail
@@ -14,14 +19,21 @@ public class Mail
     @Autowired
     private JavaMailSender javaMailSender;
 
-    public void sendEmail(EmailAddress emailAddress, ExpenseList expenseList) {
+    public void sendEmail(EmailAddress emailAddress, ExpenseList expenseList, List<String> recapVirement) {
         SimpleMailMessage msg = new SimpleMailMessage();
         msg.setTo(emailAddress.getEmailAddress());
         msg.setFrom("tricountwildschool@outlook.fr");
+        String msgVirement="";
+        for (String virement : recapVirement)
+        {
+            msgVirement=msgVirement+virement+"\n";
+        }
 
         msg.setSubject("[TriCount.com] - Traitement de l'équilibrage " + expenseList.getName());
-        msg.setText("Bonjour, \n Pour information, l'équilibrage de la liste de dépense "+ expenseList.getName() +" a été lancé avec succès. \n \n L'équipe TriCount.com");
+        msg.setText("Bonjour, \n Pour information, l'équilibrage de la liste de dépense "+ expenseList.getName() +" a été lancé avec succès." +
+                " \n \n Ci-dessous le récapitulatif des virements effectués : \n \n"  +
+                        msgVirement +
+                " \n \n  L'équipe TriCount.com");
         javaMailSender.send(msg);
     }
-
 }
